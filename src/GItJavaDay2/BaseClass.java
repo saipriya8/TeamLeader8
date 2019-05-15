052f808efb6a5a0ffcdea6b43cc2cb5dddfb8e08
saipriya8 <saipriyasusarla@gmail.com>
2019-05-15 11:57:26 +0530

    modified base class

--- src/GItJavaDay2/BaseClass.java
+++ src/GItJavaDay2/BaseClass.java
@@ -3,7 +3,7 @@ package GItJavaDay2;
 public class BaseClass {
 
 	public static void main(String[] args) {
-		System.out.println("This is Sai's Base Class");
+		System.out.println("This is modified Sai's Base Class");
 		//https://github.com/saipriya8/TeamLeader8.git
 
 	}
