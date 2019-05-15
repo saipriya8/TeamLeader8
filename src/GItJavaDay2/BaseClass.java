4856e52254b6351cadd9104ebde5d321722db85f
saipriya8 <saipriyasusarla@gmail.com>
2019-05-15 11:49:23 +0530

    added base class

---
+++ src/GItJavaDay2/BaseClass.java
@@ -0,0 +1,11 @@
+package GItJavaDay2;
+
+public class BaseClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is Sai's Base Class");
+		//https://github.com/saipriya8/TeamLeader8.git
+
+	}
+
+}
